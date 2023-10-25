d352bd8ed9fbc0215df377a58c55c73bd85537c8
Uriel Martinez <martinezuriel651@gmail.com>
2023-10-25 16:54:18 -0500

    Finished Weather Class

--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -8,3 +8,14 @@
         }
     }
 }
+
+// Display a welcome message 
+// start first day
+    // Display weather forecast 
+    // Display player inventory
+    // Allow player to go to store to buy ingredients
+    // Charge player for ingredients 
+    // Allow player to choose ingredients for the recipe
+    // Run the game
+    // Show the profit that was made and remaining ingredients
+// Start second day and repeat for seven days.
